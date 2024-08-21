547f8a3caf57844729e7765ada9eef7ef9ca9e85
guendaoui <mrfox24@gmail.com>
2024-08-22 00:13:49 +0100

    first func

--- c01.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("hellpzf rfkjtgjgt");
-}
---
+++ ft_strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+char* ft_strcpy(char* dest, const char* src)
+{
+    int i;
+
+    i = 0;
+    while(src[i] && dest[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return(dest);
+}
+int main()
+{
+    char b[] = "anass";
+    char f[] = "anasgoo";
+    char *c;
+    c = ft_strcpy(b,f);
+    printf("%s",c);
+}
